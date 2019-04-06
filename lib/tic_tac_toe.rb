72091a73889d6c26fb9eacce016b2f67c7bcde42
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-06 21:52:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,6 +51,81 @@ class TicTacToe
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+  #requests user input
+  def turn(board)
+    puts "Please choose a number 1-9:"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, player_token)
+      display_board
+    else
+      turn
+    end
+  end
+
+  #defines if there is a winning combination, draw, or if the board is empty
+  def won?(board)
+  empty_board = @board.all? {|empty| empty == " "}
+  draw = @board.all? {|token| token == "X" || token == "O"}
+  WIN_COMBINATIONS.any? do |win_combo|
+    if win_combo.all? {|index| @board[index] =="X" } || win_combo.all? {|index| @board[index] =="O"}
+       return win_combo
+    else empty_board || draw
+        false
+      end
+    end
+  end
+
+  #determines if the board is full
+  def full?
+    @board.all? { |space| space == "X" || space == "O"}
+  end
+
+  #determines if there is a draw/nobody win
+  def draw?
+    if won?
+      return false
+    elsif full?
+      return true
+    else
+      return false
+    end
+  end
+
+  #determines if the game is over by win, draw or full
+  def over?
+    if won? || draw? || full?
+      return true
+    else
+      return false
+    end
+  end
 
 
+  #determines who the winner is
+  def winner
+    index = []
+    index = won?
+      if index == false
+        return nil
+      elsif
+        @board[index[0]] == "X"
+          return "X"
+      else
+        return "O"
+      end
+  end
+
+  #Play method that initiates the loop until game won, full or draw
+  def play
+    until over? == true
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
 end
